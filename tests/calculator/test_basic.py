6710036dfd6cc5c1fbdaa7e8f56769a11ee8c42f
Rafal Kiszczyszyn <rafalkiszczyszyn09@gmail.com>
2022-05-15 01:17:30 +0200

    Add basics/sub operation

--- tests/calculator/test_basic.py
+++ tests/calculator/test_basic.py
@@ -1,8 +1,13 @@
 import unittest
 
-from src.calculator.basic import add
+from src.calculator.basic import add, sub
 
 
 class AddTests(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(add(1, 1), 2)
+        self.assertEqual(2, add(1, 1))
+
+
+class SubTests(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(0, sub(1, 1))
